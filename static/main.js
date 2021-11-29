18bb3eba67cddaba0a268ba462ba157ac32cf308
mohitkumarsharma <59123602+mohitkrsharma@users.noreply.github.com>
2021-11-29 12:27:59 +0530

    First Commit

---
+++ static/main.js
@@ -0,0 +1,30 @@
+
+console.log('Hello')
+// var socket = io();
+// socket.on('connect', function() {
+// 		socket.emit('my event', {data: 'I\'m connected!'});
+//     });
+var socket=io.connect(location.protocol+'//'+document.domain+':'+location.port);
+// socket.on('connect',function(){
+// 	socket.emit('my event',{data:'I\'m connected!'});
+// });
+
+function send(){
+	var msgBox=document.getElementById('msgBox')
+	socket.emit('msg',msgBox.value)
+	msgBox.value=""
+}
+
+socket.on('push',function(data){
+	var msgList=document.getElementById('msgList')
+	msgList.innerHTML+="<p>"+data+"</p>"
+})
+
+
+function fetchUsers(){
+	fetch('/users').then(function(res){
+		res.json().then(function(data){
+			console.log(data)
+		})
+	})
+}
